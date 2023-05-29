232df078d0535b30c52261c05bb5371286e0491a
Liangzhihe <1789211411@qq.com>
2023-05-29 22:38:40 +0800

    feat: [tag] update tag routes

--- src/routes/tagRoutes.ts
+++ src/routes/tagRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import { tagController } from '../controllers/tagController'
+import { authMiddleware } from '../utils/authmiddleware'
 
 const router = express.Router()
 
 router.get('/', tagController.getAllTags)
 
-router.post('/', tagController.createTag)
+router.post('/', authMiddleware, tagController.createTag)
 
-router.delete('/:id', tagController.deleteTag)
+router.delete('/:id', authMiddleware, tagController.deleteTag)
 
 export default router
